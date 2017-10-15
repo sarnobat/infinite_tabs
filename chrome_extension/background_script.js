bfb5db6697b906391854907a1f698fe4cc896acc
sjsu2016 <Justin@Justins-MacBook-Pro.local>
2017-10-14 20:05:04 -0700

    js files

--- chrome_extension/background_script.js
+++ chrome_extension/background_script.js
@@ -1,7 +1,17 @@
+//this file is index.js
+
 chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
   alert('extension');
 });
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-	alert('clicked');
+	alert('clicked ' + tab.id);
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  	chrome.tabs.sendMessage(tab.id, {greeting: "hello"}, function(response) {
+    console.log(response.farewell);
+  		});
+	});
+	chrome.tabs.create({ url: message.url, selected: false });
+	console.log('clicked');
+	console.debug('clicked');
 });
--- chrome_extension/content_script.js
+++ chrome_extension/content_script.js
@@ -1,4 +1,9 @@
-
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		console.debug('content-script');
+		alert('content-script');
+	}
+);
 document.body.addEventListener('click', function(arg1, arg2) {
 	console.debug("Element was clicked: " + arg1);
 }, true);
